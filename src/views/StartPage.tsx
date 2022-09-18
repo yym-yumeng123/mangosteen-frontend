2cb01a694eaade54c22292922fbbd43061552141
yym-yumeng123 <18026493873@163.com>
2022-09-18 21:30:11 +0800

    vue3 不需要传参

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,9 +4,14 @@ import s from "./StartPage.module.scss"
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const onClick = () => {
+      console.log("hi")
+    }
     return () => (
       <div class={s.button_wrapper}>
-        <Button class={s.button}>开始记账</Button>
+        <Button class={s.button} onClick={onClick}>
+          开始记账
+        </Button>
       </div>
     )
   },
