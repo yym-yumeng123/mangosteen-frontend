2a0f9c5c2f8aa007a70a18276a0df014225302af
yym-yumeng123 <18026493873@163.com>
2022-09-19 21:14:40 +0800

    完善floatButton

--- src/shared/FloatButton/FloatButton.tsx
+++ src/shared/FloatButton/FloatButton.tsx
@@ -4,6 +4,10 @@ import s from "./floatButton.module.scss"
 
 export const FloatButton = defineComponent({
   setup: (props, context) => {
-    return () => <Icon name='add' />
+    return () => (
+      <div class={s.floatButton}>
+        <Icon name='add' class={s.icon} />
+      </div>
+    )
   },
 })
