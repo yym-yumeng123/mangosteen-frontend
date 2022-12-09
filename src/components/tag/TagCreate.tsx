b1722ffb059ef76b4ab49d44c01ab3b4661a34bf
yym-yumeng123 <18026493873@163.com>
2022-12-09 22:10:47 +0800

    edit tabs

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,16 +1,10 @@
 import { defineComponent, PropType, reactive, toRaw } from "vue"
 import { MainLayout } from "../../layouts/MainLayout"
-import { Button } from "../../shared/Button/Button"
-import { EmojiSelect } from "../../shared/EmojiSelect/EmojiSelect"
 import { Icon } from "../../shared/Icon/Icon"
 import { Rules, validate } from "../../shared/validate"
-import s from "./TagCreate.module.scss"
+
+import { TagForm } from "./TagForm"
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>,
-    },
-  },
   setup: (props, context) => {
     const formData = reactive({
       name: "",
@@ -40,44 +34,7 @@ export const TagCreate = defineComponent({
         {{
           title: () => "新建标签",
           icon: () => <Icon name='left' onClick={() => {}} />,
-          default: () => (
-            <form class={s.form} onSubmit={onSubmit}>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>标签名</span>
-                  <div class={s.formItem_value}>
-                    <input
-                      v-model={formData.name}
-                      class={[s.formItem, s.input, s.error]}
-                    ></input>
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>{errors["name"] ? errors["name"][0] : "　"}</span>
-                  </div>
-                </label>
-              </div>
-              <div class={s.formRow}>
-                <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号 {formData.sign}</span>
-                  <div class={s.formItem_value}>
-                    <EmojiSelect
-                      v-model={formData.sign}
-                      class={[s.formItem, s.emojiList, s.error]}
-                    />
-                  </div>
-                  <div class={s.formItem_errorHint}>
-                    <span>{errors["sign"] ? errors["sign"][0] : "　"}</span>
-                  </div>
-                </label>
-              </div>
-              <p class={s.tips}>记账时长按标签即可进行编辑</p>
-              <div class={s.formRow}>
-                <div class={s.formItem_value}>
-                  <Button class={[s.formItem, s.button]}>确定</Button>
-                </div>
-              </div>
-            </form>
-          ),
+          default: () => <TagForm />,
         }}
       </MainLayout>
     )
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { defineComponent } from "vue"
 import { NavBar } from "../shared/Navbar/Navbar"
+import s from './MainLayout.module.scss'
 
 export const MainLayout = defineComponent({
   setup: (props, context) => {
     return () => (
-      <div>
-        <NavBar>
+      <div  class={s.wrapper}>
+        <NavBar  class={s.navbar}>
           {{
             default: () => context.slots?.title?.(),
             icon: () => context.slots?.icon?.(),
