9dda119426acf8f5d9d4bceb075797d3e746952e
yym-yumeng123 <18026493873@163.com>
2022-12-09 23:00:03 +0800

    实现 ItemList 页面样式

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,17 +1,31 @@
-import { defineComponent, PropType, ref } from "vue"
+import { defineComponent, PropType, reactive, ref } from "vue"
 import { MainLayout } from "../../layouts/MainLayout"
 import { Icon } from "../../shared/Icon/Icon"
 import { TabItem, Tabs } from "../../shared/Tabs/Tabs"
-import s from "./ItemList.module.scss"
-
+import { Time } from "../../shared/time"
+import { ItemSummary } from "./ItemSummary"
 export const ItemList = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>,
-    },
-  },
   setup: (props, context) => {
     const refSelected = ref("本月")
+    const time = new Time()
+    const customTime = reactive({
+      start: new Time(),
+      end: new Time(),
+    })
+    const timeList = [
+      {
+        start: time.firstDayOfMonth(),
+        end: time.lastDayOfMonth(),
+      },
+      {
+        start: time.add(-1, "month").firstDayOfMonth(),
+        end: time.add(-1, "month").lastDayOfMonth(),
+      },
+      {
+        start: time.firstDayOfYear(),
+        end: time.lastDayOfYear(),
+      },
+    ]
     return () => (
       <MainLayout>
         {{
@@ -22,10 +36,30 @@ export const ItemList = defineComponent({
               classPrefix={"customTabs"}
               v-model:selected={refSelected.value}
             >
-              <TabItem name='本月'>list 1</TabItem>
-              <TabItem name='上月'>list 2</TabItem>
-              <TabItem name='今年'>list 3</TabItem>
-              <TabItem name='自定义时间'>list 4</TabItem>
+              <TabItem name='本月'>
+                <ItemSummary
+                  startDate={timeList[0].start.format()}
+                  endDate={timeList[0].end.format()}
+                />
+              </TabItem>
+              <TabItem name='上月'>
+                <ItemSummary
+                  startDate={timeList[1].start.format()}
+                  endDate={timeList[1].end.format()}
+                />
+              </TabItem>
+              <TabItem name='今年'>
+                <ItemSummary
+                  startDate={timeList[2].start.format()}
+                  endDate={timeList[2].end.format()}
+                />
+              </TabItem>
+              <TabItem name='自定义时间'>
+                <ItemSummary
+                  startDate={customTime.start.format()}
+                  endDate={customTime.end.format()}
+                />
+              </TabItem>
             </Tabs>
           ),
         }}
