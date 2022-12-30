e8bbe270084bb7e1e20633f372366e6194e14e33
yym-yumeng123 <18026493873@163.com>
2022-12-30 18:30:30 +0800

    可以新增记账了

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,39 @@
 /// <reference types="vite/client" />
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+declare module "*.scss" {
+  const content: Record<string, any> = {}
+  export default content
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
   const component: DefineComponent<{}, {}, any>
   export default component
 }
 
-type JSONValue = null | boolean | string | number | JSONValue[] | Record<string, JSONValue>
+type JSONValue =
+  | null
+  | boolean
+  | string
+  | number
+  | JSONValue[]
+  | Record<string, JSONValue>
 
 type Tag = {
-  id: number,
-  user_id: number,
-  name: string,
-  sign: string,
+  id: number
+  user_id: number
+  name: string
+  sign: string
+  kind: expenses | income
+}
+
+type Item = {
+  id: number
+  user_id: number
+  amount: number
+  tags_id: number[]
+  happen_at: string
   kind: expenses | income
 }
 
@@ -24,4 +44,12 @@ type Resources<T> = {
     per_page: number
     count: number
   }
+}
+
+type Resource<T> = {
+  resource: T
+}
+
+type ResourceError = {
+  errors: Record<string, string[]>
 }
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -1,17 +1,29 @@
-import { faker } from "@faker-js/faker"
-import { AxiosRequestConfig } from "axios"
+import { faker } from '@faker-js/faker'
+import { AxiosRequestConfig } from 'axios';
 
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
-faker.setLocale("zh_CN")
+faker.setLocale('zh_CN');
 
+export const mockItemCreate: Mock = config => {
+  return [200, {
+    resource: {
+      "id": 2264,
+      "user_id": 1312,
+      "amount": 9900,
+      "note": null,
+      "tags_id": [3508],
+      "happen_at": "2020-10-29T16:00:00.000Z",
+      "created_at": "2022-07-03T15:35:56.301Z",
+      "updated_at": "2022-07-03T15:35:56.301Z",
+      "kind": "expenses"
+    }
+  }]
+}
 export const mockSession: Mock = (config) => {
-  return [
-    200,
-    {
-      jwt: faker.random.word(),
-    },
-  ]
+  return [200, {
+    jwt: faker.random.word()
+  }]
 }
 
 let id = 0
@@ -19,16 +31,12 @@ const createId = () => {
   id += 1
   return id
 }
-
 export const mockTagIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
   const count = 26
-
   const createPaper = (page = 1) => ({
-    page,
-    per_page,
-    count,
+    page, per_page, count
   })
   const createTag = (n = 1, attrs?: any) =>
     Array.from({ length: n }).map(() => ({
@@ -36,20 +44,20 @@ export const mockTagIndex: Mock = (config) => {
       name: faker.lorem.word(),
       sign: faker.internet.emoji(),
       kind: config.params.kind,
-      ...attrs,
+      ...attrs
     }))
   const createBody = (n = 1, attrs?: any) => ({
-    resources: createTag(n),
-    pager: createPaper(page),
+    resources: createTag(n), pager: createPaper(page)
   })
 
-  if (kind === "expenses" && (!page || page === 1)) {
+  if (kind === 'expenses' && (!page || page === 1)) {
     return [200, createBody(25)]
-  } else if (kind === "expenses" && page === 2) {
+  } else if (kind === 'expenses' && page === 2) {
     return [200, createBody(1)]
-  } else if (kind === "income" && (!page || page === 1)) {
+  } else if (kind === 'income' && (!page || page === 1)) {
     return [200, createBody(25)]
   } else {
     return [200, createBody(1)]
   }
+
 }
