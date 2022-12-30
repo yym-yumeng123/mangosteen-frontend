d2fec02195666dc9b829f0072503123716e64020
yym-yumeng123 <18026493873@163.com>
2022-12-30 16:18:57 +0800

    使用 mock

--- src/shared/Http.tsx
+++ src/shared/Http.tsx
@@ -1,11 +1,15 @@
-import axios, { AxiosError, AxiosInstance, AxiosRequestConfig } from "axios"
-type JSONValue =
-  | string
-  | number
-  | null
-  | boolean
-  | JSONValue[]
-  | { [key: string]: JSONValue }
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios"
+import { mockSession, mockTagIndex } from "../mock/mock"
+
+type GetConfig = Omit<AxiosRequestConfig, "params" | "url" | "method">
+type PostConfig = Omit<AxiosRequestConfig, "url" | "data" | "method">
+type PatchConfig = Omit<AxiosRequestConfig, "url" | "data">
+type DeleteConfig = Omit<AxiosRequestConfig, "params">
 
 export class Http {
   instance: AxiosInstance
@@ -14,11 +18,10 @@ export class Http {
       baseURL,
     })
   }
-  // read
   get<R = unknown>(
     url: string,
-    query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, "params" | "url" | "method">
+    query?: Record<string, JSONValue>,
+    config?: GetConfig
   ) {
     return this.instance.request<R>({
       ...config,
@@ -27,27 +30,24 @@ export class Http {
       method: "get",
     })
   }
-  // create
   post<R = unknown>(
     url: string,
     data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, "url" | "data" | "method">
+    config?: PostConfig
   ) {
     return this.instance.request<R>({ ...config, url, data, method: "post" })
   }
-  // update
   patch<R = unknown>(
     url: string,
     data?: Record<string, JSONValue>,
-    config?: Omit<AxiosRequestConfig, "url" | "data">
+    config?: PatchConfig
   ) {
     return this.instance.request<R>({ ...config, url, data, method: "patch" })
   }
-  // destroy
   delete<R = unknown>(
     url: string,
     query?: Record<string, string>,
-    config?: Omit<AxiosRequestConfig, "params">
+    config?: DeleteConfig
   ) {
     return this.instance.request<R>({
       ...config,
@@ -58,26 +58,57 @@ export class Http {
   }
 }
 
+const mock = (response: AxiosResponse) => {
+  if (
+    location.hostname !== "localhost" &&
+    location.hostname !== "127.0.0.1" &&
+    location.hostname !== "192.168.3.57"
+  ) {
+    return false
+  }
+  switch (response.config?.params?._mock) {
+    case "tagIndex":
+      [response.status, response.data] = mockTagIndex(response.config)
+      return true
+    case "session":
+      [response.status, response.data] = mockSession(response.config)
+      return true
+  }
+  return false
+}
+
 export const http = new Http("/api/v1")
 
-http.instance.interceptors.request.use(config => {
-  const jwt = localStorage.getItem('jwt')
-  if(jwt) {
+http.instance.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem("jwt")
+  if (jwt) {
     config.headers!.Authorization = `Bearer ${jwt}`
   }
-
   return config
 })
 
-http.instance.interceptors.response.use(response=>{
-  console.log('response')
-  return response
-}, (error) => {
-  if(error.response){
-    const axiosError = error as AxiosError
-    if(axiosError.response?.status === 429){
-      alert('你太频繁了')
+http.instance.interceptors.response.use(
+  (response) => {
+    mock(response)
+    return response
+  },
+  (error) => {
+    if (mock(error.response)) {
+      return error.response
+    } else {
+      throw error
     }
   }
-  throw error
-})
+)
+http.instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      const axiosError = error as AxiosError
+      if (axiosError.response?.status === 429) {
+        alert("你太频繁了")
+      }
+    }
+    throw error
+  }
+)
