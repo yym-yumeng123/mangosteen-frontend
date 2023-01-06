3733eb9894613bcb55083adf6d26499375b9e291
yym-yumeng123 <18026493873@163.com>
2023-01-06 20:48:20 +0800

    创建标签

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, onUpdated, PropType } from "vue"
+import { RouterLink } from "vue-router"
 import { Button } from "../../shared/Button/Button"
 import { http } from "../../shared/Http"
 import { Icon } from "../../shared/Icon/Icon"
@@ -14,9 +15,10 @@ export const Tags = defineComponent({
   },
   emits: ["update:selected"],
   setup: (props, context) => {
+    const { kind } = props
     const { tags, hasMore, page, fetchTags } = useTags((page) => {
       return http.get<Resources<Tag>>("/tags", {
-        kind: props.kind,
+        kind,
         page: page + 1,
         _mock: "tagIndex",
       })
@@ -27,12 +29,12 @@ export const Tags = defineComponent({
     return () => (
       <>
         <div class={s.tags_wrapper}>
-          <div class={s.tag}>
+          <RouterLink to={`/tags/create?kind=${kind}`} class={s.tag}>
             <div class={s.sign}>
               <Icon name='add' class={s.createTag} />
             </div>
             <div class={s.name}>新增</div>
-          </div>
+          </RouterLink>
           {tags.value.map((tag) => (
             <div
               class={[s.tag, props.selected === tag.id ? s.selected : ""]}
