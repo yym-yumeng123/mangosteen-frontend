98265ca9007af5e3dec04201da5e74b4f33dbd15
yym-yumeng123 <18026493873@163.com>
2023-01-07 16:18:51 +0800

    优化 dataBetter

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -26,24 +26,24 @@ export const Charts = defineComponent({
   setup: (props, context) => {
     const kind = ref("expenses")
     const data1 = ref<Data1>([])
-    const betterData1 = computed<[string, number][]>(()=> {
-      if(!props.startDate || !props.endDate) {
+    const betterData1 = computed<[string, number][]>(() => {
+      if (!props.startDate || !props.endDate) {
         return []
       }
-      const array = []
-      const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
+      const diff =
+        new Date(props.endDate).getTime() - new Date(props.startDate).getTime()
       const n = diff / DAY + 1
-      let data1Index = 0
-      for(let i=0; i<n; i++) {
-        const time = new Time(props.startDate+'T00:00:00.000+0800').add(i, 'day').getTimestamp()
-        if(data1.value[data1Index] && new Date(data1.value[data1Index].happen_at).getTime() === time){
-          array.push([new Date(time).toISOString(), data1.value[data1Index].amount])
-          data1Index += 1
-        }else{
-          array.push([new Date(time).toISOString(), 0])
-        }
-      }
-      return array as [string, number][]
+      return Array.from({ length: n }).map((_, i) => {
+        const time = new Time(props.startDate + "T00:00:00.000+0800")
+          .add(i, "day")
+          .getTimestamp()
+        const item = data1.value[0]
+        const amount =
+          item && new Date(item.happen_at).getTime() === time
+            ? data1.value.shift()!.amount
+            : 0
+        return [new Date(time).toISOString(), amount]
+      })
     })
 
     onMounted(async () => {
