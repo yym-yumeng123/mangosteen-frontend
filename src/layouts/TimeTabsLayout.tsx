09353458211161d0a40c56373d990669588739c0
yym-yumeng123 <18026493873@163.com>
2023-01-07 15:05:32 +0800

    每次切换tab重新渲染图表

--- src/layouts/TimeTabsLayout.tsx
+++ src/layouts/TimeTabsLayout.tsx
@@ -32,6 +32,10 @@ export const TimeTabsLayout = defineComponent({
       type: Object as PropType<typeof demo>,
       required: true,
     },
+    rerenderOnSwitchTab: {
+      type: Boolean,
+      default: false
+    }
   },
   setup: (props, context) => {
     const refSelected = ref("本月")
@@ -80,6 +84,7 @@ export const TimeTabsLayout = defineComponent({
                 classPrefix='customTabs'
                 v-model:selected={refSelected.value}
                 onUpdate:selected={onSelect}
+                rerenderOnSelect={props.rerenderOnSwitchTab}
               >
                 <TabItem name='本月'>
                   <props.component
