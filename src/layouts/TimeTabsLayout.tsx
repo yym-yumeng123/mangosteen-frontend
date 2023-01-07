f57fcf0abe3ebd16de0a4643b2eb18d2d9233bf6
yym-yumeng123 <18026493873@163.com>
2023-01-07 16:28:21 +0800

    取消显示今年的图标

--- src/layouts/TimeTabsLayout.tsx
+++ src/layouts/TimeTabsLayout.tsx
@@ -30,15 +30,19 @@ export const TimeTabsLayout = defineComponent({
   props: {
     component: {
       type: Object as PropType<typeof demo>,
-      required: true,
+      required: true
     },
     rerenderOnSwitchTab: {
+      type: Boolean,
+      default: false
+    },
+    hideThisYear: {
       type: Boolean,
       default: false
     }
   },
   setup: (props, context) => {
-    const refSelected = ref("本月")
+    const refSelected = ref('本月')
     const time = new Time()
     const tempTime = reactive({
       start: new Time().format(),
@@ -51,16 +55,16 @@ export const TimeTabsLayout = defineComponent({
     const timeList = [
       {
         start: time.firstDayOfMonth(),
-        end: time.lastDayOfMonth(),
+        end: time.lastDayOfMonth()
       },
       {
-        start: time.add(-1, "month").firstDayOfMonth(),
-        end: time.add(-1, "month").lastDayOfMonth(),
+        start: time.add(-1, 'month').firstDayOfMonth(),
+        end: time.add(-1, 'month').lastDayOfMonth()
       },
       {
         start: time.firstDayOfYear(),
-        end: time.lastDayOfYear(),
-      },
+        end: time.lastDayOfYear()
+      }
     ]
     const refOverlayVisible = ref(false)
     const onSubmitCustomTime = (e: Event) => {
@@ -69,72 +73,68 @@ export const TimeTabsLayout = defineComponent({
       Object.assign(customTime, tempTime)
     }
     const onSelect = (value: string) => {
-      if (value === "自定义时间") {
+      if (value === '自定义时间') {
         refOverlayVisible.value = true
       }
     }
     return () => (
       <MainLayout>
         {{
-          title: () => "山竹记账",
+          title: () => '山竹记账',
           icon: () => <OverlayIcon />,
           default: () => (
             <>
-              <Tabs
-                classPrefix='customTabs'
-                v-model:selected={refSelected.value}
-                onUpdate:selected={onSelect}
-                rerenderOnSelect={props.rerenderOnSwitchTab}
-              >
-                <TabItem name='本月'>
-                  <props.component
-                    startDate={timeList[0].start.format()}
-                    endDate={timeList[0].end.format()}
-                  />
-                </TabItem>
-                <TabItem name='上月'>
-                  <props.component
-                    startDate={timeList[1].start.format()}
-                    endDate={timeList[1].end.format()}
-                  />
-                </TabItem>
-                <TabItem name='今年'>
-                  <props.component
-                    startDate={timeList[2].start.format()}
-                    endDate={timeList[2].end.format()}
-                  />
-                </TabItem>
-                <TabItem name='自定义时间'>
-                  <props.component
-                    startDate={customTime.start}
-                    endDate={customTime.end}
-                  />
-                </TabItem>
-              </Tabs>
+              {props.hideThisYear ? (
+                <Tabs
+                  classPrefix="customTabs"
+                  v-model:selected={refSelected.value}
+                  onUpdate:selected={onSelect}
+                  rerenderOnSelect={props.rerenderOnSwitchTab}
+                >
+                  <TabItem name="本月">
+                    <props.component startDate={timeList[0].start.format()} endDate={timeList[0].end.format()} />
+                  </TabItem>
+                  <TabItem name="上月">
+                    <props.component startDate={timeList[1].start.format()} endDate={timeList[1].end.format()} />
+                  </TabItem>
+                  <TabItem name="自定义时间">
+                    <props.component startDate={customTime.start} endDate={customTime.end} />
+                  </TabItem>
+                </Tabs>
+              ) : (
+                <Tabs
+                  classPrefix="customTabs"
+                  v-model:selected={refSelected.value}
+                  onUpdate:selected={onSelect}
+                  rerenderOnSelect={props.rerenderOnSwitchTab}
+                >
+                  <TabItem name="本月">
+                    <props.component startDate={timeList[0].start.format()} endDate={timeList[0].end.format()} />
+                  </TabItem>
+                  <TabItem name="上月">
+                    <props.component startDate={timeList[1].start.format()} endDate={timeList[1].end.format()} />
+                  </TabItem>
+                  <TabItem name="今年">
+                    <props.component startDate={timeList[2].start.format()} endDate={timeList[2].end.format()} />
+                  </TabItem>
+                  <TabItem name="自定义时间">
+                    <props.component startDate={customTime.start} endDate={customTime.end} />
+                  </TabItem>
+                </Tabs>
+              )}
               <Overlay show={refOverlayVisible.value} class={s.overlay}>
                 <div class={s.overlay_inner}>
                   <header>请选择时间</header>
                   <main>
                     <Form onSubmit={onSubmitCustomTime}>
-                      <FormItem
-                        label='开始时间'
-                        v-model={tempTime.start}
-                        type='date'
-                      />
-                      <FormItem
-                        label='结束时间'
-                        v-model={tempTime.end}
-                        type='date'
-                      />
+                      <FormItem label="开始时间" v-model={tempTime.start} type="date" />
+                      <FormItem label="结束时间" v-model={tempTime.end} type="date" />
                       <FormItem>
                         <div class={s.actions}>
-                          <button
-                            type='button'
-                            onClick={() => (refOverlayVisible.value = false)}
-                          >
+                          <button type="button" onClick={() => (refOverlayVisible.value = false)}>
                             取消
                           </button>
-                          <button type='submit'>确认</button>
+                          <button type="submit">确认</button>
                         </div>
                       </FormItem>
                     </Form>
@@ -142,9 +142,9 @@ export const TimeTabsLayout = defineComponent({
                 </div>
               </Overlay>
             </>
-          ),
+          )
         }}
       </MainLayout>
     )
-  },
+  }
 })
--- src/views/StatisticsPage/StatisticsPage.tsx
+++ src/views/StatisticsPage/StatisticsPage.tsx
@@ -1,10 +1,14 @@
-import { defineComponent } from 'vue';
-import { Charts } from '../../components/statistics/Charts';
-import { TimeTabsLayout } from '../../layouts/TimeTabsLayout';
+import { defineComponent } from "vue"
+import { Charts } from "../../components/statistics/Charts"
+import { TimeTabsLayout } from "../../layouts/TimeTabsLayout"
 export const StatisticsPage = defineComponent({
   setup: (props, context) => {
     return () => (
-      <TimeTabsLayout rerenderOnSwitchTab={true} component={Charts} />
+      <TimeTabsLayout
+        rerenderOnSwitchTab={true}
+        component={Charts}
+        hideThisYear={true}
+      />
     )
-  }
+  },
 })
