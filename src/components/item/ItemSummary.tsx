5fee1b3943ccf33e1b07f8f6a1cec4d9f9b7d5bf
yym-yumeng123 <18026493873@163.com>
2023-01-07 12:53:33 +0800

    封装 Money

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -2,6 +2,7 @@ import { defineComponent, onMounted, PropType, ref } from "vue"
 import { Button } from "../../shared/Button/Button"
 import { FloatButton } from "../../shared/FloatButton/FloatButton"
 import { http } from "../../shared/Http"
+import { Money } from "../../shared/Money/Money"
 import s from "./ItemSummary.module.scss"
 export const ItemSummary = defineComponent({
   props: {
@@ -63,7 +64,7 @@ export const ItemSummary = defineComponent({
                     <div class={s.tagAndAmount}>
                       <span class={s.tag}>{item.tags_id[0]}</span>
                       <span class={s.amount}>
-                        ￥<>{item.amount}</>
+                        ￥<><Money value={item.amount}/></>
                       </span>
                     </div>
                     <div class={s.time}>{item.happen_at}</div>
