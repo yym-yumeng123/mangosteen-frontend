c4b1ffa8ca32e133da1d4d5d5a774a34670d5b13
yym-yumeng123 <18026493873@163.com>
2023-01-07 16:34:29 +0800

    完善饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -9,6 +9,8 @@ import { Time } from "../../shared/time"
 
 type Data1Item = { happen_at: string; amount: number }
 type Data1 = Data1Item[]
+type Data2Item = { tag_id: number; tag: Tag; amount: number }
+type Data2 = Data2Item[]
 
 const DAY = 24 * 3600 * 1000
 
@@ -53,12 +55,33 @@ export const Charts = defineComponent({
           happen_after: props.startDate,
           happen_before: props.endDate,
           kind: kind.value,
+          group_by: 'happen_at',
           _mock: "itemSummary",
         }
       )
       console.log("response.data")
       console.log(response.data)
       data1.value = response.data.groups
+    })
+
+    
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map((item) => ({
+        name: item.tag.name,
+        value: item.amount
+      }))
+    )
+
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
     })
     return () => (
       <div class={s.wrapper}>
@@ -72,7 +95,7 @@ export const Charts = defineComponent({
           v-model={kind.value}
         />
         <LineChart data={betterData1.value} />
-        <PieChart />
+        <PieChart  data={betterData2.value} />
         <Bars />
       </div>
     )
