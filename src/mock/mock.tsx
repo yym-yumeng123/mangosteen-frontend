2bb3a0086bf92bd7b5f05bbf8507a1c2d31f1a83
yym-yumeng123 <18026493873@163.com>
2023-01-07 12:36:37 +0800

    加载 items 列表

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,18 +5,47 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale("zh_CN")
 
-export const mockTagEdit: Mock = config => {
-  const createTag = (attrs?: any) =>
-    ({
+export const mockItemIndex: Mock = (config) => {
+  const { kind, page } = config.params
+  const per_page = 25
+  const count = 26
+  const createPaper = (page = 1) => ({
+    page,
+    per_page,
+    count,
+  })
+  const createItem = (n = 1, attrs?: any) =>
+    Array.from({ length: n }).map(() => ({
       id: createId(),
-      name: faker.lorem.word(),
-      sign: faker.internet.emoji(),
-      kind: 'expenses',
-      ...attrs
-    })
-  return [200, {resource: createTag()}]
+      user_id: createId(),
+      amount: Math.floor(Math.random() * 10000),
+      tags_id: [createId()],
+      happen_at: faker.date.past().toISOString(),
+      kind: config.params.kind,
+    }))
+  const createBody = (n = 1, attrs?: any) => ({
+    resources: createItem(n),
+    pager: createPaper(page),
+  })
+  if (!page || page === 1) {
+    return [200, createBody(25)]
+  } else if (page === 2) {
+    return [200, createBody(1)]
+  } else {
+    return [200, {}]
+  }
 }
 
+export const mockTagEdit: Mock = (config) => {
+  const createTag = (attrs?: any) => ({
+    id: createId(),
+    name: faker.lorem.word(),
+    sign: faker.internet.emoji(),
+    kind: "expenses",
+    ...attrs,
+  })
+  return [200, { resource: createTag() }]
+}
 
 export const mockTagShow: Mock = (config) => {
   const createTag = (attrs?: any) => ({
