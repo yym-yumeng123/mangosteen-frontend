0d4de39a5e68b7e56903d0c4115b106cdaa44ee6
yym-yumeng123 <18026493873@163.com>
2023-05-16 19:27:50 +0800

    UI优化

--- src/shared/ComingSoon/ComingSoon.tsx
+++ src/shared/ComingSoon/ComingSoon.tsx
@@ -2,6 +2,8 @@ import { defineComponent, PropType } from "vue"
 import { Center } from "../Center/Center"
 import { Icon } from "../Icon/Icon"
 import s from "./ComingSoon.module.scss"
+import { Button } from "../Button/Button"
+import { useRouter } from "vue-router"
 
 export const ComingSoon = defineComponent({
   props: {
@@ -10,12 +12,19 @@ export const ComingSoon = defineComponent({
     },
   },
   setup: (props, context) => {
+    const router = useRouter()
+    const onClick = () => {
+      router.back()
+    }
     return () => (
       <div>
         <Center class={s.pig_wrapper}>
           <Icon name='pig' class={s.pig} />
         </Center>
         <p class={s.text}>敬请期待</p>
+        <p class={s.link} >
+          <Button onClick={onClick}>返回</Button>
+        </p>
       </div>
     )
   },
